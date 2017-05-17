5511b9dd7a26098e4f112f1e3b8bff395ccd0444
Ram Ochoa <ram.ochoa.hes@gmail.com>
2017-05-17 01:11:56 -0500

    fixing server

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'a4'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', 'host'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,17 @@ Route::get('/debug', function() {
         running on your production server, making your credentials public.
         */
  //print_r(config('database.connections.mysql'));
+
+ echo '<h1>Test Database Connection</h1>';
+ try {
+  $results = DB::select('SHOW DATABASES;');
+  echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+  echo "<br><br>Your Databases:<br><br>";
+  print_r($results);
+ }
+ catch (Exception $e) {
+  echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+ }
+
+ echo '</pre>';
+ }
